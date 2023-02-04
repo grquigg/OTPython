072b0ff1533396b956b6257be021ab755ade3884
Greg Quigg <gquigg@umass.edu>
2023-02-04 14:57:21 -0500

    Got optimize.minimize working for basic example

--- predict.py
+++ predict.py
@@ -24,34 +24,18 @@ def load_data(file, delimiter='\t'):
 
 def compute_probabilities(constraint_weights, data):
     data = np.array(data).astype(float)
-    freqs = data[:,2:]
+    freqs = data[:,0:1]
     h = -np.dot(constraint_weights, data[:,0:2])
     h_soft = softmax(h)
 
 def calculate_data_likelihood(constraint_weights, data):
     data = np.array(data).astype(float)
-    freqs = data[:,2:]
-    h = -np.dot(constraint_weights, data[:,0:2])
+    freqs = data[:,0:1]
+    h = -np.dot(constraint_weights, data[:,1:3])
     h_soft = softmax(h)
     log_candidate_probs = np.log(h_soft)
     log_probs = np.sum(np.dot(log_candidate_probs, freqs))
     return log_probs, h_soft
-
-def compute_gradient(weights, probs, data, bias_params):
-    freqs = data[:,2:]
-    constraint_data = data[:,:2]
-    dot = np.dot(np.transpose(probs), constraint_data)
-    mult = np.multiply(freqs, constraint_data)
-    total_constraints = np.sum(mult, axis=0)
-    loss = total_constraints - dot[0]
-    if(bias_params != None):
-        mus = bias_params[0]
-        sigmas = bias_params[1]
-        top = (weights - mus)
-        bottom = 2 * sigmas**2
-        bias = np.sum(top / bottom)
-        loss += bias
-    return loss
 
 def calculate_bias(mus, sigmas, constraint_weights):
     top = (constraint_weights - mus)**2
@@ -59,18 +43,24 @@ def calculate_bias(mus, sigmas, constraint_weights):
     bias = np.sum(top / bottom)
     return bias
 
-def log_likelihood(constraint_weights, args, bias_params):
+def log_likelihood(constraint_weights, data, bias_params):
     l = 0
-    data = args[0]
-    full_data = args[1]
-    total_probs = np.empty((0,1))
+    total_probs = []
     for key, value in data.items():
         loss, probs = calculate_data_likelihood(constraint_weights, value)
         l += loss
-        total_probs = np.concatenate([total_probs, np.transpose(probs)])
+        total_probs = np.concatenate([total_probs, probs])
     if(bias_params != None):
         l += - calculate_bias(bias_params[0], bias_params[1], constraint_weights)
-    return l, total_probs
+    print(l)
+    return -l
+
+def predict(constraint_weights, data, bias_params):
+    total_probs = []
+    for key, value in data.items():
+        loss, probs = calculate_data_likelihood(constraint_weights, value)
+        total_probs = np.concatenate([total_probs, probs])
+    return total_probs
 
 def convert(weights, data):
     weights = weights.flatten()
@@ -89,7 +79,7 @@ def load_bias_file(bias_file, sep="\t"):
     print(sigmas)
     return (mus, sigmas)
 
-def optimize(input_file, bias_file=None, constraint_weights=None, learning_rate=0.1, iterations=1000):
+def optimize(input_file, bias_file=None, constraint_weights=None, learning_rate=0.1, iterations=1):
 
     input = load_data(input_file)
     if(bias_file != None):
@@ -101,14 +91,10 @@ def optimize(input_file, bias_file=None, constraint_weights=None, learning_rate=
     data = input[2]
     full_data = input[3]
     weights = np.ones((1, len(long_names)))
-
     #optimization code
-    for i in range(iterations):
-        log, probs = log_likelihood(weights, (data, full_data,), bias_params)
-        print(log)
-        gradient = compute_gradient(weights, probs, full_data, bias_params)
-        weights = weights - learning_rate * gradient
-    print(weights)
-
+    result = minimize(log_likelihood, weights, args=(data,bias_params,), options={'xatol': 1e-8, 'disp': True})
+    new_weights = np.reshape(result.x, (1,2))
+    probs = predict(result.x, data, bias_params)
+    print(probs)
 
 optimize("sample_data_file.txt", bias_file="sample_constraint_file.txt")
