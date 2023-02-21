93c7be4d06005e855c7bc9539cc4e0d0660af22e
Greg Quigg <gquigg@umass.edu>
2023-02-21 15:31:10 -0500

    Fix minor bug in inference.py

--- inference.py
+++ inference.py
@@ -14,6 +14,7 @@ def generate_combinations(constraints, i, j, k, num_features):
                 num_hash = convert_vector_to_num(array, num_features)
                 if(num_hash not in constraints):
                     constraints[num_hash] = array
+
 def generate_constraint_dict(num_features, max_features=3): #these are only going to be one consonant at a time
     pass
     constraints = {}
@@ -22,12 +23,12 @@ def generate_constraint_dict(num_features, max_features=3): #these are only goin
             for k in range(num_features):
                 generate_combinations(constraints, i, j, k, num_features)
     return constraints
-
+#don't include "repeat" constraints
 def generate_full_constraint_space(constraints, possible_constraints):
     constraint_space = {}
-    for i in range(len(possible_constraints)):
-        for j in range(len(possible_constraints)):
-            for k in range(len(possible_constraints)):
+    for i in range(1,len(possible_constraints)):
+        for j in range(1,len(possible_constraints)):
+            for k in range(1,len(possible_constraints)):
                 value = (constraints[possible_constraints[i]] + 
                          constraints[possible_constraints[j]] +
                          constraints[possible_constraints[k]])
@@ -72,10 +73,12 @@ for i in range(consonants.shape[0]):
 consonants_dict = {}
 for consonant in consonants:
     consonants_dict[consonant[0]] = consonant[1:]
-
+NUM_FEATURES = len(consonant_headers)-1
 featurized = featurize_data(data, consonants_dict)
-constraints =  generate_constraint_dict(len(consonant_headers)-1)
+constraints =  generate_constraint_dict(NUM_FEATURES)
 possible_constraints = [key for key in constraints.keys()]
+#this is formally a stack
+
 #this takes way too long
 # full_constraints = generate_full_constraint_space(constraints, possible_constraints)
 #convert data features
@@ -83,30 +86,37 @@ violation_table = np.zeros((len(data), len(possible_constraints)), dtype=int)
 
 
 #evaluate constraint violations
-for j in range(len(possible_constraints)):
+for j in range(2, 3):
     value = constraints[possible_constraints[j]]
     #this is where things get hairy with the math
     for i in range(len(data)):
         val = featurized[data[i][0]]
-        #multiply the vector representations of constraint and value
-        #in the case of this specific problem, candidates that violate a constraint will be marked by 
-        #either 1 or 4 (1^2 and 2^2, respectively)
-        result = np.multiply(value, val)
-        #if 1 or 4 in result, then the constraint is violated
-        #however, it needs to violate all parts of the constraint
-        if(1 in result or 4 in result):
-            violation = True
-            for i in range(len(value)):
-                #if the two values are not the same and the values 
-                if(value[i] != val[i] and (value[i] == 1 or value[i] == 2)):
-                    violation = False
-            if(violation):
-                violation_table[i][j] += 1
+        print(val)
+        for k in range(0, len(val), NUM_FEATURES):
+            #multiply the vector representations of constraint and value
+            #in the case of this specific problem, candidates that violate a constraint will be marked by 
+            #either 1 or 4 (1^2 and 2^2, respectively)
+            print(val[k:k+NUM_FEATURES])
+            print(value)
+            result = np.multiply(value, val[k:k+NUM_FEATURES])
+            print("Result")
+            print(result)
+            #if 1 or 4 in result, then the constraint is violated
+            #however, it needs to violate all parts of the constraint
+            if(1 in result or 4 in result):
+                violation = True
+                for n in range(len(value)):
+                    #if the two values are not the same and the values 
+                    if(value[n] != val[n] and (value[n] == 1 or value[n] == 2)):
+                        violation = False
+                if(violation):
+                    print("violation")
+                    violation_table[i][j] += 1
 print("DONE")
-sample = random.randint(1,1000)
 total_violations = np.sum(violation_table, axis=0)
 print(total_violations)
 #now we get the full constraint space
 
-#TO-DO: Figure out whether there's a way to simplify constraint space at least a small amount
-
+#TO-DO: Figure out whether there's a way to simplify constraint space a significant amount
+#determine which constraints actually might have violations based on our data
+#TO-DO: produce output file called blick
